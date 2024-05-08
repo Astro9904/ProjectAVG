a92086344b35163fb010194d3cfde875bf916b8f
AstroWoof <ryanjvanes@gmail.com>
2024-05-08 13:49:06 +0200

    sync

--- src/lib/customio.c
+++ src/lib/customio.c
@@ -1,5 +1,3 @@
 #include <avr/io.h>
 #include "customio.h"
 
-
-
--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 #include "lib/customio.h"
 #include <avr/io.h>
 
-
 int main() 
 {
-    
+	IO_t D13 = {DDRB, PORTB, PB7};
+	* D13.DDR |= _BV(D13.SBS);
+	* D13.IOR |= _BV(D13.SBS);
+	
 
 }

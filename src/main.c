c615af34b0d8d8303f8f70854c90c9da20637fbf
AstroWoof <ryanjvanes@gmail.com>
2024-05-14 12:33:45 +0200

    Added pinmode , digitalread, digitalwrite functions to custom io.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include "lib/customio.h"
-#include <avr/io.h>
+#include "lib/ESD.h"
 
 int main() 
 {
-	IO_t D13 = {DDRB, PORTB, PB7};
-	* D13.DDR |= _BV(D13.SBS);
-	* D13.IOR |= _BV(D13.SBS);
-	
-
+	IO_t D7 = {&DDRH, &PORTH, PH4};
+	IO_t D8 = {&DDRH, &PORTH, PH5};
+	IO_t D4 = {&DDRG, &PORTG, PG5};
+	cio_pinMode(&D7, OUTPUT);
+	cio_digitalWrite(&D7, HIGH);
 }
